4638bdc5d78c3510081abfcdc80c2193478c8405
dragongaming1 <76481853+dragongaming1@users.noreply.github.com>
2021-04-21 15:52:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('NzkwOTI4MTgwNDkwNzk3MDg3.X-HvRA.9MoDtn__jHyO8bi7jnrkUZbIHwI');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
